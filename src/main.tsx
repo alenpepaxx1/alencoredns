03b6ffc8524d3f880c763ad086ea2ff718e8fe35
Alen Pepa <xalenpepa@gmail.com>
2026-06-21 05:54:15 -0700

    Add files via upload

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+// Copyright Alen Pepa
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
